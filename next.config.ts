64c3a705d1231152bbe996b3dae820271db51638
Prabesh Uprety <prabeshuprety@Prabeshs-MacBook-Air.local>
2025-06-05 10:47:07 +0545

    add: gooten host as config

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "az412349.cdn.gooten.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
